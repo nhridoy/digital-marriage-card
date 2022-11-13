4d82223abdbddc12720a87fe7d044edec883f0c5
Nahidujjaman Hridoy <hridoyboss12@gmail.com>
2022-11-13 13:44:02 +0600

    fontsouce poppins
    Signed-off-by: Nahidujjaman Hridoy <hridoyboss12@gmail.com>

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import "@fontsource/poppins";
 import "./index.css";
 import { registerSW } from "virtual:pwa-register";
 import ReloadPrompt from "./components/pwa/ReloadPrompt";
