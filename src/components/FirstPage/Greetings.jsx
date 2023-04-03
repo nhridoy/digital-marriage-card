8804a575600e67fa1427b2d64d619f6a803031cd
Nahidujjaman Hridoy <hridoyboss12@gmail.com>
2023-04-03 12:44:08 +0600

    popup
    Signed-off-by: Nahidujjaman Hridoy <hridoyboss12@gmail.com>

--- src/components/FirstPage/Greetings.jsx
+++ src/components/FirstPage/Greetings.jsx
@@ -7,16 +7,38 @@ const Greetings = () => {
     <div className="flex flex-col justify-between h-screen">
       <div className="mt-2 text-6xl text-pink-700 font-dancing">Greetings</div>
       <div className="flex justify-between px-4 mb-2">
-        <img
-          src={bride}
-          alt="Bride"
-          className="w-24 transform -scale-x-100 md:w-44"
-        />
-        <img
-          src={groom}
-          alt="Groom"
-          className="w-24 transform -scale-x-100 md:w-44"
-        />
+        <div className="relative">
+          <img
+            src={bride}
+            alt="Bride"
+            className="w-24 transform -scale-x-100 md:w-44"
+          />
+          <div className="absolute top-0 p-2 bg-white rounded-md -right-28">
+            Pull me to you...
+            <div
+              className="absolute left-0 w-0 h-0 
+  border-t-[10px] border-t-transparent
+  border-r-[25px] border-r-white
+  border-b-[10px] border-b-transparent -rotate-[32deg] -translate-x-3 -translate-y-1"
+            ></div>
+          </div>
+        </div>
+        <div className="relative">
+          <img
+            src={groom}
+            alt="Groom"
+            className="w-24 transform -scale-x-100 md:w-44"
+          />
+          <div className="absolute top-0 p-2 bg-white rounded-md -left-28">
+            Come Closer...
+            <div
+              className="absolute right-0 w-0 h-0 
+  border-t-[10px] border-t-transparent
+  border-r-[25px] border-r-white
+  border-b-[10px] border-b-transparent rotate-[212deg] translate-x-3 -translate-y-1"
+            ></div>
+          </div>
+        </div>
       </div>
     </div>
   );
