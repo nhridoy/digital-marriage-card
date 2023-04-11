aa6ea415954de0a435abadbf1ebb01b19937ccbf
Nahidujjaman Hridoy <hridoyboss12@gmail.com>
2023-04-11 07:47:47 +0600

    location updated
    Signed-off-by: Nahidujjaman Hridoy <hridoyboss12@gmail.com>

--- src/components/FirstPage/Map.jsx
+++ src/components/FirstPage/Map.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Map = () => {
   return (
     <a
-      href="https://goo.gl/maps/b9KrD971u7ioZJ2C7"
+      href="https://goo.gl/maps/divUgqSrwoDYgmBs6"
       target={"_blank"}
       className="bg-pink-400 p-2 rounded-md text-white hover:text-white focus:text-white hover:bg-pink-500"
     >
